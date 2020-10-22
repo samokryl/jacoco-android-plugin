c352108a85691b2da2e4dd54bb2dfbf2b1768878
Alexander Krylov <a.krilov@cft.ru>
2020-10-22 13:32:57 +0700

    JAP-1 CreateFullReportTask was added

--- src/main/kotlin/ru/samokryl/jacoco/android/extension/JacocoAndroidExtension.kt
+++ src/main/kotlin/ru/samokryl/jacoco/android/extension/JacocoAndroidExtension.kt
@@ -3,4 +3,6 @@ package ru.samokryl.jacoco.android.extension
 open class JacocoAndroidExtension {
 
 	var tool: String = ""
+
+	var destinationFile: String? = null
 }
--- src/main/kotlin/ru/samokryl/jacoco/android/task/CreateFullReportTask.kt
+++ src/main/kotlin/ru/samokryl/jacoco/android/task/CreateFullReportTask.kt
@@ -1,5 +1,6 @@
 package ru.samokryl.jacoco.android.task
 
+import org.gradle.api.tasks.Input
 import org.gradle.testing.jacoco.tasks.JacocoReport
 import ru.samokryl.jacoco.android.extension.JacocoAndroidExtension
 
@@ -7,9 +8,33 @@ open class CreateFullReportTask : JacocoReport() {
 
 	init {
 		group = "Jacoco"
+		description = "Create jacoco report for all modules"
 	}
 
-	fun setUp(jacocoAndroidExtension: JacocoAndroidExtension) {
+	@Input
+	lateinit var jacocoAndroidExtension: JacocoAndroidExtension
 
+	fun configure(jacocoAndroidExtension: JacocoAndroidExtension) {
+		val destinationFile = jacocoAndroidExtension.destinationFile
+			?: throw IllegalArgumentException("JacocoAndroidExtension.destinationFile hasn't set")
+
+		executionData(project.files(destinationFile))
+
+		reports.apply {
+			xml.apply {
+				isEnabled = true
+				destination = project.file("${project.buildDir}/reports/jacoco/jacoco.xml")
+			}
+			csv.apply {
+				isEnabled = true
+				destination = project.file("${project.buildDir}/reports/jacoco/jacoco.csv")
+			}
+			html.apply {
+				isEnabled = true
+				destination = project.file("${project.buildDir}/reports/jacoco")
+			}
+		}
+
+		classDirectories.setFrom(project.files("${project.buildDir}/jacoco"))
 	}
 }
